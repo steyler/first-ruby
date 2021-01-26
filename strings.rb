3cd2972e44ea891dc84df0b0683b7c493cbce6ff
Steyler <steylerap@gmail.com>
2021-01-26 17:03:45 -0600

    First ruby project

---
+++ strings.rb
@@ -0,0 +1,12 @@
+a= "esto es un strin "
+b= "otro string"
+#en ruby casi todo es un objeto
+
+puts a.length
+
+puts b.length
+
+# string de multiples lineas
+
+c= " esto es\nun string\nde multiples lineas"
+puts c 
---
+++ variables.rb
@@ -0,0 +1,10 @@
+#var let o cons le da igual
+a= 37
+
+a+1
+
+puts a
+puts a = a+1
+
+a= a+15
+puts a
